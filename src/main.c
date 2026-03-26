7ab4976d34432975243de401c62bf4a0b33b6d47
Owen <ocpearsall@hotmail.com>
2026-03-26 14:49:19 -0500

    Added CLI interface

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#include "lib/optparse99/optparse99.h"
 #include "lib/hashmap.c/hashmap.h"
 
 #include "verbum/token/token.h"
@@ -8,11 +9,65 @@
 #include "verbum/parser/parser.h"
 #include "verbum/code/code.h"
 
+/* Helper macro for default `--help` option */
+#define CLI_HELP_OPTION .short_name = 'h',\
+			.long_name = "help",\
+			.description = "displays this page",\
+			.function = optparse_print_help,
 
-int main() {
-	Parser *p = parser_new("c.ebnf");
-	AST *ast = parser_parse(p);
-	Analyzer *a = analyzer_new(ast);
+
+void verbum_generate(int argc, char **argv);
+
+int main(int argc, char *argv[]) {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+	struct optparse_cmd verbum = {
+		.name = "verbum",
+		.about = "verbum 0.1.0 - ebnf to c parser generator",
+		.description = "verbum is an ebnf to c parser generator that produces good code.",
+		.usage = "verbum <SUBCOMMAND> [OPTIONS]",
+		.options = (struct optparse_opt []) {
+			{ CLI_HELP_OPTION },
+			{ END_OF_OPTIONS },
+		},
+		.subcommands = (struct optparse_cmd []) {
+			(struct optparse_cmd) {
+				.name = "generate",
+				.about = "generates parser",
+				.description = "generates parser",
+				.usage = "generate <FILE>",
+				.options = (struct optparse_opt []) {
+					{ CLI_HELP_OPTION },
+					{ END_OF_OPTIONS },
+				},
+				.function = verbum_generate,
+			},
+			{ END_OF_SUBCOMMANDS },
+		},
+
+
+	};
+
+#pragma GCC diagnostic pop
+
+	optparse_parse(&verbum, &argc, &argv);
+
+	return 0;
+}
+
+void verbum_generate(int argc, char **argv) {
+	Parser *p = NULL;
+	AST *ast = NULL;
+	Analyzer *a = NULL;
+
+	if(argc != 2) {
+		fprintf(stderr, "Expected file got none!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	p = parser_new(argv[1]);
+	ast = parser_parse(p);
+	a = analyzer_new(ast);
 
 	analyzer_analyze(a);
 
@@ -84,5 +139,5 @@ int main() {
 	ast_delete(ast);
 	parser_delete(p);
 
-	return 0;
+
 }
